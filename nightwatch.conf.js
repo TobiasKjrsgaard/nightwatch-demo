bee1ac6eff6bdef1b83b90b9eed1814a5058b615
Tobias Møller Kjærsgaard <tmkj@betterstudents.com>
2017-03-18 11:37:11 +0100

    Add .editorconfig and convert all tabs indents to spaces

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,40 +1,40 @@
 var seleniumServer = require('selenium-server');
 
 module.exports = {
-	"src_folders": ["tests"],
-	"output_folder": "reports",
-	"custom_commands_path": "",
-	"custom_assertions_path": "",
-	"page_objects_path": "page-objects",
-	"globals_path": "",
+  "src_folders": ["tests"],
+  "output_folder": "reports",
+  "custom_commands_path": "",
+  "custom_assertions_path": "",
+  "page_objects_path": "page-objects",
+  "globals_path": "",
 
-	"selenium": {
-		"start_process": true,
-		"server_path": seleniumServer.path,
-		"host": "127.0.0.1",
-		"port": 4444
-	},
+  "selenium": {
+    "start_process": true,
+    "server_path": seleniumServer.path,
+    "host": "127.0.0.1",
+    "port": 4444
+  },
 
-	"test_settings": {
-		"default": {
-			"launch_url": "http://dev.matthewroach.me/login/",
-			"selenium_host": "localhost",
-			"selenium_port": 4444,
-			"pathname": "/wd/hub",
-			"silent": true,
-			"screenshots": {
-				"enabled": false,
-				"path": ""
-			},
-			"desiredCapabilities": {
-				"browserName": "chrome"
-			}
-		},
+  "test_settings": {
+    "default": {
+      "launch_url": "http://dev.matthewroach.me/login/",
+      "selenium_host": "localhost",
+      "selenium_port": 4444,
+      "pathname": "/wd/hub",
+      "silent": true,
+      "screenshots": {
+        "enabled": false,
+        "path": ""
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      }
+    },
 
-		"ci": {
-			"desiredCapabilities": {
-				"browserName": "firefox"
-			}
-		}
-	}
+    "ci": {
+      "desiredCapabilities": {
+        "browserName": "firefox"
+      }
+    }
+  }
 }
