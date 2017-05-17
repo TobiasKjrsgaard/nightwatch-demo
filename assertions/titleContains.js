2fffed4920f67dd4a8020879d80e55ab689829e7
Tobias Møller Kjærsgaard <tmkj@betterstudents.com>
2017-05-17 14:02:00 +0200

    Added a new custom assertion to see if title contians a string

---
+++ assertions/titleContains.js
@@ -0,0 +1,35 @@
+/**
+ * Checks if the page title contains the given value.
+ *
+ * ```
+ *    this.demoTest = function (client) {
+ *      browser.assert.titleContains("Nightwatch");
+ *    };
+ * ```
+ *
+ * @method title
+ * @param {string} expected The expected page title substring.
+ * @param {string} [message] Optional log message to display in the output. If missing, one is displayed by default.
+ * @api assertions
+ */
+
+var util = require('util');
+exports.assertion = function(expected, msg) {
+
+  this.message = msg || util.format('Testing if the page title contains "%s".', expected);
+  this.expected = expected;
+
+  this.pass = function(value) {
+    return value.indexOf(this.expected) > -1;
+  };
+
+  this.value = function(result) {
+    return result.value;
+  };
+
+  this.command = function(callback) {
+    this.api.title(callback);
+    return this;
+  };
+
+};
