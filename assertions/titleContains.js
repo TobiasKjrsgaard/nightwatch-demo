3fcacad3124fd5eca3b08b4635e4814a6c44a2e0
Tobias Møller Kjærsgaard <tmkj@betterstudents.com>
2017-05-18 14:13:30 +0200

    Add JSDoc comments to explain what's going on

--- assertions/titleContains.js
+++ assertions/titleContains.js
@@ -16,17 +16,44 @@
 var util = require('util');
 exports.assertion = function(expected, msg) {
 
+  /**
+   * The message which will be used in the test output and
+   * inside the XML reports
+   * @type {string}
+   */
   this.message = msg || util.format('Testing if the page title contains "%s".', expected);
+
+  /**
+   * A value to perform the assertion on. If a function is
+   * defined, its result will be used.
+   * @type {function|*}
+   */
   this.expected = expected;
 
+  /**
+   * The method which performs the actual assertion. It is
+   * called with the result of the value method as the argument.
+   * @type {function}
+   */
   this.pass = function(value) {
     return value.indexOf(this.expected) > -1;
   };
 
+  /**
+   * The method which returns the value to be used on the
+   * assertion. It is called with the result of the command's
+   * callback as argument.
+   * @type {function}
+   */
   this.value = function(result) {
     return result.value;
   };
 
+  /**
+   * Performs a protocol command/action and its result is
+   * passed to the value method via the callback argument.
+   * @type {function}
+   */
   this.command = function(callback) {
     this.api.title(callback);
     return this;
